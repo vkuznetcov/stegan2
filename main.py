edc662f3a48e0b2b5a557804ed301077af03c52b
denis <denis.sabirzianow@yandex.ru>
2022-10-25 14:22:51 +0400

    PAF

--- main.py
+++ main.py
@@ -8,120 +8,115 @@ from utils.snipping import get_H_zone, merge_pictures_H_zone, split_image_to_4_p
 from watermark import generate_watermark, proximity_measure, builtin_watermark
 
 
-def get_dataset(container, m, sigma, alpha, beta, key):
+def get_dataset(image, m, sigma, alpha, beta, key):
+    # 2. Get fft of image
+    fft_container = get_fft_image(image)
 
-     # 2. Get fft of image
-     fft_container = get_fft_image(container)
+    # 3. Get abs of image (+ phase)
+    abs_fft_container = get_abs_matrix(fft_container)
+    phase_fft_container = get_phase_matrix(fft_container)
 
-     # 3. Get abs of image (+ phase)
-     abs_fft_container = get_abs_matrix(fft_container)
-     phase_fft_container = get_phase_matrix(fft_container)
+    # 4. Snipping
+    H_zone = get_H_zone(abs_fft_container)
 
-     # 4. Snipping
-     H_zone = get_H_zone(abs_fft_container)
-     # For Pavel !!!
-     # split_image_to_4_parts...
+    # 5. Get watermark
+    watermark_length = H_zone.shape[0] * H_zone.shape[1]
+    # нормальное распределение
+    watermark = generate_watermark(watermark_length, m, sigma, key)[0].reshape(H_zone.shape[0], H_zone.shape[1])
 
-     # 5. Get watermark
-     watermark_length = H_zone.shape[0] * H_zone.shape[1]
-     watermark = generate_watermark(watermark_length, m, sigma, key)[0].reshape(H_zone.shape[0], H_zone.shape[1])
+    # 6. Embedding (аддитивное)
+    H_zone_abs_container_with_watermark = additional_embedding(H_zone, beta, watermark, alpha)
 
-     # 6. Embedding
-     H_zone_abs_container_with_watermark = multiplication_embedding(H_zone, beta, watermark, alpha)
+    # 7. Merge pictures
+    abs_container_with_watermark = merge_pictures_H_zone(abs_fft_container, H_zone_abs_container_with_watermark)
 
-     # 7. Merge pictures
-     abs_container_with_watermark = merge_pictures_H_zone(abs_fft_container, H_zone_abs_container_with_watermark)
+    # 8. Recover complex matrix
+    complex_container_with_watermark = get_complex_matrix(abs_container_with_watermark, phase_fft_container)
 
-     # 8. Recover complex matrix
-     complex_container_with_watermark = get_complex_matrix(abs_container_with_watermark, phase_fft_container)
-
-     # 9. ifft
-     return get_inverse_fft_image(complex_container_with_watermark)
-
-
+    # 9. ifft
+    return get_inverse_fft_image(complex_container_with_watermark)
 
 
 def get_optimal_parameter(initial_container, target_value=0.9, max_possible_value=1.0):
-     alpha = 0.01
-     alphas = list()
-     omega = list()
-     omega_tilda = list()
+    alpha = 0.01
+    alphas = list()
+    omega = list()
+    omega_tilda = list()
 
-     rho_max = 0
-     alpha_max = 0
+    rho_max = 0
+    alpha_max = 0
 
-     while alpha < max_possible_value:
+    while alpha < max_possible_value:
 
-          print(f'step {alpha}')
-          # 1. Get H zone from changed picture
-          changed_container = get_dataset(initial_container, M, SIGMA, alpha, BETA, KEY)
-          abs_fft_recover = get_abs_matrix(get_fft_image(changed_container))
-          H_zone_recover = get_H_zone(abs_fft_recover)
+        print(f'step {alpha}')
+        # 1. Get H zone from changed picture
+        changed_container = get_dataset(initial_container, M, SIGMA, alpha, BETA, KEY)
+        abs_fft_recover = get_abs_matrix(get_fft_image(changed_container))
+        H_zone_recover = get_H_zone(abs_fft_recover)
 
-          # 2. Get H zone from initial picture
-          abs_fft_container = get_abs_matrix(get_fft_image(initial_container))
-          H_zone = get_H_zone(abs_fft_container)
+        # 2. Get H zone from initial picture
+        abs_fft_container = get_abs_matrix(get_fft_image(initial_container))
+        H_zone = get_H_zone(abs_fft_container)
 
-          new_shape = [1, H_zone_recover.shape[0] * H_zone_recover.shape[1]]
-          watermark = generate_watermark(new_shape[1], M, SIGMA, KEY)[0]
-          watermark_tilda = builtin_watermark(
-                                       H_zone_recover.reshape(new_shape[0], new_shape[1]),
-                                       H_zone.reshape(new_shape[0], new_shape[1]),
-                                       ALPHA)
-          rho = proximity_measure(watermark.reshape(new_shape[0], new_shape[1]), watermark_tilda)
+        new_shape = [1, H_zone_recover.shape[0] * H_zone_recover.shape[1]]
+        watermark = generate_watermark(new_shape[1], M, SIGMA, KEY)[0]
+        watermark_tilda = builtin_watermark(
+            H_zone_recover.reshape(new_shape[0], new_shape[1]),
+            H_zone.reshape(new_shape[0], new_shape[1]),
+            ALPHA)
+        rho = proximity_measure(watermark.reshape(new_shape[0], new_shape[1]), watermark_tilda)
 
-          if rho > rho_max:
-               rho_max = rho
-               alpha_max = alpha
+        if rho > rho_max:
+            rho_max = rho
+            alpha_max = alpha
 
-          if rho >= target_value:
-               alphas.append(alpha)
-               omega.append(watermark)
-               omega_tilda.append(watermark_tilda)
-          alpha += 0.01
+        if rho >= target_value:
+            alphas.append(alpha)
+            omega.append(watermark)
+            omega_tilda.append(watermark_tilda)
+        alpha += 0.01
 
-     if len(alphas) == 0:
-          return alpha_max
+    if len(alphas) == 0:
+        return alpha_max
 
-     alpha_min = alphas[0]
-     PSNR_min = cv2.PSNR(omega[0], omega_tilda[0])
+    alpha_min = alphas[0]
+    PSNR_min = cv2.PSNR(omega[0], omega_tilda[0])
 
-     for i in range (1, len(alphas)):
-         PSNR = cv2.PSNR(omega[i], omega_tilda[i])
-         if PSNR_min > PSNR:
-              PSNR_min = PSNR
-              alpha_min = alphas[i]
+    for i in range(1, len(alphas)):
+        PSNR = cv2.PSNR(omega[i], omega_tilda[i])
+        if PSNR_min > PSNR:
+            PSNR_min = PSNR
+            alpha_min = alphas[i]
 
-     return alpha_min
+    return alpha_min
 
 
 def get_rho():
-     fft_container = get_fft_image(container)
+    fft_container = get_fft_image(container)
 
-     # 3. Get abs of image (+ phase)
-     abs_fft_container = get_abs_matrix(fft_container)
+    # 3. Get abs of image (+ phase)
+    abs_fft_container = get_abs_matrix(fft_container)
 
-     # 4. Snipping
-     H_zone = get_H_zone(abs_fft_container)
+    # 4. Snipping
+    H_zone = get_H_zone(abs_fft_container)
 
-     # 5.
+    # 5.
 
-     new_shape = [1, H_zone.shape[0] * H_zone.shape[1]]
+    new_shape = [1, H_zone.shape[0] * H_zone.shape[1]]
 
-     result_image = read_image('resource/result.png')
-     fft_recover = get_fft_image(result_image)
-     abs_fft_recover = get_abs_matrix(fft_recover)
-     H_zone_recover = get_H_zone(abs_fft_recover).reshape(new_shape[0], new_shape[1])
+    result_image = read_image('resource/result.png')
+    fft_recover = get_fft_image(result_image)
+    abs_fft_recover = get_abs_matrix(fft_recover)
+    H_zone_recover = get_H_zone(abs_fft_recover).reshape(new_shape[0], new_shape[1])
 
+    watermark_length = H_zone.shape[0] * H_zone.shape[1]
+    watermark = generate_watermark(watermark_length, 300, 10, KEY)[0]
 
-     watermark_length = H_zone.shape[0] * H_zone.shape[1]
-     watermark = generate_watermark(watermark_length, 300, 10, KEY)[0]
+    H_zone = H_zone.reshape(new_shape[0], new_shape[1])
 
-     H_zone = H_zone.reshape(new_shape[0], new_shape[1])
+    reshaped_watermark = watermark.reshape(new_shape[0], new_shape[1])
 
-     reshaped_watermark = watermark.reshape(new_shape[0], new_shape[1])
-
-     return proximity_measure(reshaped_watermark, builtin_watermark(H_zone_recover, H_zone, ALPHA))
+    return proximity_measure(reshaped_watermark, builtin_watermark(H_zone_recover, H_zone, ALPHA))
 
 
 def different_fragments(parts, result_image, watermark):
@@ -133,90 +128,86 @@ def different_fragments(parts, result_image, watermark):
     new_shape = [1, int(H_zone_recover.shape[0] / 2) * int(H_zone_recover.shape[1] / 2)]
 
     for i in range(0, 4, 1):
-         watermark_tilda = builtin_watermark(recover_parts[i].reshape(new_shape[0], new_shape[1]),
-                                                   parts[i].reshape(new_shape[0], new_shape[1]),
-                                                   ALPHA)
-         watermark = watermark.reshape(new_shape[0], new_shape[1])
-         watermark_tilda = watermark_tilda.reshape(new_shape[0], new_shape[1])
-         rho = proximity_measure(watermark,
-                                 watermark_tilda)
-         psnr = cv2.PSNR(watermark, watermark_tilda)
-         print(f'Result {i}: Rho={rho}; PSNR={psnr}')
-
+        watermark_tilda = builtin_watermark(recover_parts[i].reshape(new_shape[0], new_shape[1]),
+                                            parts[i].reshape(new_shape[0], new_shape[1]),
+                                            ALPHA)
+        watermark = watermark.reshape(new_shape[0], new_shape[1])
+        watermark_tilda = watermark_tilda.reshape(new_shape[0], new_shape[1])
+        rho = proximity_measure(watermark,
+                                watermark_tilda)
+        psnr = cv2.PSNR(watermark, watermark_tilda)
+        print(f'Result {i}: Rho={rho}; PSNR={psnr}')
 
 
 if __name__ == '__main__':
 
-     # # 1. Get image
-     container = read_image('resource/bridge.tif')
-     #
-     # result_image = get_dataset(container, 300, 10, ALPHA, BETA, KEY)
-     #
-     # # 10. Save
-     # write_image(result_image, 'resource/result.png')
+    # # 1. Get image
+    container = read_image('resource/bridge.tif')
+    #
+    # result_image = get_dataset(container, 300, 10, ALPHA, BETA, KEY)
+    #
+    # # 10. Save
+    # write_image(result_image, 'resource/result.png')
 
-     # Task 6 =====================================================================================
-     # 1
+    # Task 6 =====================================================================================
+    # 1
 
-     # # 2. Get fft of image
-     # fft_container = get_fft_image(container)
-     #
-     # # 3. Get abs of image (+ phase)
-     # abs_fft_container = get_abs_matrix(fft_container)
-     #
-     # # 4. Snipping
-     # H_zone = get_H_zone(abs_fft_container)
-     #
-     # # 5.
-     #
-     # new_shape = [1, H_zone.shape[0] * H_zone.shape[1]]
-     #
-     # result_image = read_image('resource/result.png')
-     # fft_recover = get_fft_image(result_image)
-     # abs_fft_recover = get_abs_matrix(fft_recover)
-     # H_zone_recover = get_H_zone(abs_fft_recover).reshape(new_shape[0], new_shape[1])
-     #
-     #
-     # watermark_length = H_zone.shape[0] * H_zone.shape[1]
-     # watermark = generate_watermark(watermark_length, 300, 10, KEY)[0]
-     #
-     # H_zone = H_zone.reshape(new_shape[0], new_shape[1])
-     #
-     # reshaped_watermark = watermark.reshape(new_shape[0], new_shape[1])
-     #
-     # prox_measure = proximity_measure(reshaped_watermark, builtin_watermark(H_zone_recover, H_zone, ALPHA))
-     # print(f'Proximity measure: {prox_measure}')
+    # # 2. Get fft of image
+    # fft_container = get_fft_image(container)
+    #
+    # # 3. Get abs of image (+ phase)
+    # abs_fft_container = get_abs_matrix(fft_container)
+    #
+    # # 4. Snipping
+    # H_zone = get_H_zone(abs_fft_container)
+    #
+    # # 5.
+    #
+    # new_shape = [1, H_zone.shape[0] * H_zone.shape[1]]
+    #
+    # result_image = read_image('resource/result.png')
+    # fft_recover = get_fft_image(result_image)
+    # abs_fft_recover = get_abs_matrix(fft_recover)
+    # H_zone_recover = get_H_zone(abs_fft_recover).reshape(new_shape[0], new_shape[1])
+    #
+    #
+    # watermark_length = H_zone.shape[0] * H_zone.shape[1]
+    # watermark = generate_watermark(watermark_length, 300, 10, KEY)[0]
+    #
+    # H_zone = H_zone.reshape(new_shape[0], new_shape[1])
+    #
+    # reshaped_watermark = watermark.reshape(new_shape[0], new_shape[1])
+    #
+    # prox_measure = proximity_measure(reshaped_watermark, builtin_watermark(H_zone_recover, H_zone, ALPHA))
+    # print(f'Proximity measure: {prox_measure}')
 
-     # +====================================================================================================
+    # +====================================================================================================
 
-     fft_container = get_fft_image(container)
-     abs_fft_container = get_abs_matrix(fft_container)
-     phase_fft_container = get_phase_matrix(fft_container)
+    fft_container = get_fft_image(container)
+    abs_fft_container = get_abs_matrix(fft_container)
+    phase_fft_container = get_phase_matrix(fft_container)
 
-     H_zone = get_H_zone(abs_fft_container)
-     initial_parts = split_image_to_4_parts(H_zone)
-     watermark_length = initial_parts[0].shape[0] * initial_parts[0].shape[1]
-     watermark = generate_watermark(watermark_length, 300, 10, KEY)[0]
+    H_zone = get_H_zone(abs_fft_container)
+    initial_parts = split_image_to_4_parts(H_zone)
+    watermark_length = initial_parts[0].shape[0] * initial_parts[0].shape[1]
+    watermark = generate_watermark(watermark_length, 300, 10, KEY)[0]
 
-     for i in range (0, 4, 1):
-          initial_parts[i] = multiplication_embedding(initial_parts[i], BETA, watermark.reshape(initial_parts[i].shape[0], initial_parts[i].shape[1]), ALPHA)
+    for i in range(0, 4, 1):
+        initial_parts[i] = multiplication_embedding(initial_parts[i], BETA, watermark.reshape(initial_parts[i].shape[0],
+                                                                                              initial_parts[i].shape[
+                                                                                                  1]), ALPHA)
 
-     abs_container_with_watermark = merge_pictures_H_zone_parts(abs_fft_container, initial_parts)
-     complex_container_with_watermark = get_complex_matrix(abs_container_with_watermark, phase_fft_container)
-     result_image = get_inverse_fft_image(complex_container_with_watermark)
-     write_image(result_image, 'resource/Paul.png')
+    abs_container_with_watermark = merge_pictures_H_zone_parts(abs_fft_container, initial_parts)
+    complex_container_with_watermark = get_complex_matrix(abs_container_with_watermark, phase_fft_container)
+    result_image = get_inverse_fft_image(complex_container_with_watermark)
+    write_image(result_image, 'resource/Paul.png')
 
-     result_image = read_image('resource/result.png')
-     abs_fft_container = get_abs_matrix(get_fft_image(container))
-     H_zone = get_H_zone(abs_fft_container)
-     initial_parts = split_image_to_4_parts(H_zone)
+    # result_image = read_image('resource/result.png')
+    abs_fft_container = get_abs_matrix(get_fft_image(container))
+    H_zone = get_H_zone(abs_fft_container)
+    initial_parts = split_image_to_4_parts(H_zone)
 
+    different_fragments(initial_parts, result_image, watermark)
 
-     different_fragments(initial_parts, result_image, watermark)
-
-
-
-     #alpha_result = get_optimal_parameter(container)
-     #print(f'{alpha_result}')
-
-
+    # alpha_result = get_optimal_parameter(container)
+    # print(f'{alpha_result}')
--- utils/snipping.py
+++ utils/snipping.py
@@ -2,7 +2,6 @@ import numpy as np
 
 
 def get_H_zone(image, modifier=0.25):
-
     image_shape = image.shape
     result_shape = (image_shape[0] * modifier, image_shape[1] * modifier)
     y_border_size = result_shape[1]
@@ -29,9 +28,9 @@ def split_image_to_4_parts(image):
 
 
 def merge_pictures_H_zone(image_source, snipped_part, modifier=0.25):
-
+    # modifier - 1/4 часть от image
     image_shape = image_source.shape
-    result_shape = (image_shape[0] * modifier, image_shape[1] * modifier)
+    result_shape = (image_shape[0] * modifier, image_shape[1] * modifier)  # размер H (64x64)
     y_border_size = result_shape[1]
     x_border_size = result_shape[0]
 
@@ -42,17 +41,16 @@ def merge_pictures_H_zone(image_source, snipped_part, modifier=0.25):
     lower_border = int(image_shape[1] - y_border_size - 1)
 
     result = np.copy(image_source)
-    result[left_border:right_border, upper_border:lower_border] = snipped_part
+    result[left_border:right_border, upper_border:lower_border] = snipped_part  # snipped_part - H зона
 
     return result
 
 
 def merge_pictures_H_zone_parts(image, snipped_parts):
-
     fake_H_zone = get_H_zone(image)
     x_center, y_center = int(fake_H_zone.shape[0] / 2), int(fake_H_zone.shape[1] / 2)
 
-    #result = np.copy(image)
+    # result = np.copy(image)
     fake_H_zone[0:x_center, 0:y_center] = snipped_parts[0]
     fake_H_zone[0:x_center, y_center:image.shape[1]] = snipped_parts[1]
     fake_H_zone[x_center:image.shape[0], 0:y_center] = snipped_parts[2]
